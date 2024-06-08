ea4b17555de34849d039dd13131eb031558fb2bc
RavitHun <hunravit2711@gmail.com>
2024-06-08 23:12:20 +0700

    initial Spring boot project

---
+++ src/main/java/ravit/dev/api/controller/AppController.java
@@ -0,0 +1,15 @@
+package ravit.dev.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class AppController {
+  
+  @GetMapping()
+  public String getMethodName() {
+      return "Test";
+  }
+  
+}
---
+++ src/test/java/ravit/dev/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ravit.dev.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
